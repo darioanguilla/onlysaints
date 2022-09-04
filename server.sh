c35f87dbf0c42cbaf06c03f42e1a27e9d9a68710
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-04 17:01:54 +0200

    Revert "Incoming req to /dev/null"
    This reverts commit c5b972f7f5f6b08085e4dbd8842981e25b49d7f7.

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while true; do
-	printf "HTTP/1.0 200 OK\nContent-Length: `wc index.html | awk '{print $3-1}'`\n\n`cat index.html`" | nc -l 9191 > /dev/null
+	printf "HTTP/1.0 200 OK\nContent-Length: `wc index.html | awk '{print $3-1}'`\n\n`cat index.html`" | nc -l 9191
 done;
