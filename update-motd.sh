b928af63602a41a8aa9a84e8f2fadeb9f442f975
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-04 16:53:25 +0200

    update-saints not found in dk

--- update-motd.sh
+++ update-motd.sh
@@ -6,4 +6,4 @@ COL=$(wc -L ./motd | cut -d " " -f 1);
 /usr/bin/echo "" >> ./motd;
 /usr/bin/curl -s -H "Accept: application/json" https://www.vaticannews.va/it/santo-del-giorno/`/usr/bin/date +"%m/%d"`.saints.js | /usr/bin/jq -r '.saints[] | "\"\(.name)\"\n\n\(.summary)\n"'| iconv -f utf-8 -t ascii//translit | fold -w$COL | awk '{$1=$1};1' >> ./motd;
 /usr/bin/cat ./motd | /usr/bin/boxes -d parchment > ./sotd;
-./update-saints.sh;
+update-saints.sh;
