939d5bda957f92d48ac38d4c49e643db1bc4c796
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-04 21:30:20 +0200

    -k on curl

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:latest
 RUN apt-get update \
     && apt-get install -y --no-install-recommends bash git figlet toilet jq boxes curl locales netcat-openbsd \
     && locale-gen it_IT.UTF-8 \
-    && git config --global http.sslverify "false" && git clone https://github.com/darioanguilla/onlysaints.git
+    && git config --global http.sslverify "false" && git clone -b feature/docker --single-branch https://github.com/darioanguilla/onlysaints.git
 WORKDIR /onlysaints
 RUN ./update-motd.sh
 EXPOSE 9191
--- update-motd.sh
+++ update-motd.sh
@@ -4,6 +4,6 @@ COL=0
 /usr/bin/date +"%a, %d%b%Y" | /usr/bin/toilet -f big > ./motd;
 COL=$(wc -L ./motd | cut -d " " -f 1);
 /usr/bin/echo "" >> ./motd;
-/usr/bin/curl -s -H "Accept: application/json" https://www.vaticannews.va/it/santo-del-giorno/`/usr/bin/date +"%m/%d"`.saints.js | /usr/bin/jq -r '.saints[] | "\"\(.name)\"\n\n\(.summary)\n"'| iconv -f utf-8 -t ascii//translit | fold -w$COL | awk '{$1=$1};1' >> ./motd;
+/usr/bin/curl -k -s -H "Accept: application/json" https://www.vaticannews.va/it/santo-del-giorno/`/usr/bin/date +"%m/%d"`.saints.js | /usr/bin/jq -r '.saints[] | "\"\(.name)\"\n\n\(.summary)\n"'| iconv -f utf-8 -t ascii//translit | fold -w$COL | awk '{$1=$1};1' >> ./motd;
 /usr/bin/cat ./motd | /usr/bin/boxes -d parchment > ./sotd;
 ./update-saints.sh
