6ebadd8c5c9003061a5087c6f66b9db46d4825cd
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-05 10:36:36 +0200

    Revert

--- server.sh
+++ server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-./update-motd.sh
 while true; do
 	kill -9 $(ps -a | grep nc | awk '{print $1}')
 	printf "HTTP/1.0 200 OK\nContent-Length: `wc index.html | awk '{print $3-1}'`\n\n`cat index.html`" | nc -l 9191 > /dev/null
