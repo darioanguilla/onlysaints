536260de3ef17a4230cf7dd9e49b0fde0db6c5fe
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 22:02:31 +0200

    msg on restart

--- restart.sh
+++ restart.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 if ps | grep nc > /dev/null; then
+    echo "Killing nc";
 	kill -9 $(ps | grep nc | awk '{print $1}')
 fi
 
-if ps | grep server.sh > /dev/null; then
-            kill -9 $(ps | grep server.sh | awk '{print $1}')
+if ps | grep server.sh > /dev/null; then    
+    echo "Killing server.sh";
+    kill -9 $(ps | grep server.sh | awk '{print $1}')
 fi
 
 if ps | grep start.sh > /dev/null; then
-            kill -9 $(ps | grep start.sh | awk '{print $1}')
+    echo "Killing start.sh";
+    kill -9 $(ps | grep start.sh | awk '{print $1}')
 fi
 
 if ps | grep restart.sh | awk 'NR==2{print $1}' > /dev/null; then
-            kill -9 $(ps | grep restart.sh | awk 'NR==1{print $1}')
+    echo "Killing restart.sh";
+    kill -9 $(ps | grep restart.sh | awk 'NR==1{print $1}')
 fi
 
 /onlysaints/start.sh
