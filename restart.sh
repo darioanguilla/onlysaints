6704bbaaea279e0ea46207356e38b8e92ad83f94
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 21:52:17 +0200

    ps

--- restart.sh
+++ restart.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-if ps -ax | grep nc > /dev/null; then
-	kill -9 $(ps -ax | grep nc | awk '{print $1}')
+if ps | grep nc > /dev/null; then
+	kill -9 $(ps | grep nc | awk '{print $1}')
 fi
 
-if ps -ax | grep server.sh > /dev/null; then
-            kill -9 $(ps -ax | grep server.sh | awk '{print $1}')
+if ps | grep server.sh > /dev/null; then
+            kill -9 $(ps | grep server.sh | awk '{print $1}')
 fi
 
-if ps -ax | grep start.sh > /dev/null; then
-            kill -9 $(ps -ax | grep start.sh | awk '{print $1}')
+if ps | grep start.sh > /dev/null; then
+            kill -9 $(ps | grep start.sh | awk '{print $1}')
 fi
 
-if ps -ax | grep restart.sh | awk 'NR==2{print $1}' > /dev/null; then
-            kill -9 $(ps -ax | grep restart.sh | awk 'NR==1{print $1}')
+if ps | grep restart.sh | awk 'NR==2{print $1}' > /dev/null; then
+            kill -9 $(ps | grep restart.sh | awk 'NR==1{print $1}')
 fi
 
 /onlysaints/start.sh
