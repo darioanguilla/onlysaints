619c83e45deef5ddced1777119ac7d8381cb9fdd
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 22:24:30 +0200

    modified if in restart and server

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    if [[ $(ps | grep nc)]]; then
+    if [[ $(ps | grep nc)]] then
 	    kill -9 $(ps -a | grep nc | awk '{print $1}')
 	fi
 	printf "HTTP/1.0 200 OK\nContent-Length: `wc index.html | awk '{print $3-1}'`\n\n`cat index.html`" | nc -l 9191 > /dev/null
