8ed9e3df5a537ae74514e4ee62ff69def3ae2458
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 22:38:52 +0200

    modified if in restart and server

--- restart.sh
+++ restart.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-if [$(ps | grep nc) != ""]; then
+if $(ps | grep nc) != "" then
     echo "Killing nc";
 	kill -9 $(ps | grep nc | awk '{print $1}')
 fi
 
-if [$(ps | grep server.sh) != ""]; then    
+if $(ps | grep server.sh) != "" then    
     echo "Killing server.sh";
     kill -9 $(ps | grep server.sh | awk '{print $1}')
 fi
 
-if [$(ps | grep start.sh) != ""]; then
+if $(ps | grep start.sh) != "" then
     echo "Killing start.sh";
     kill -9 $(ps | grep start.sh | awk '{print $1}')
 fi
 
-if [$(ps | grep restart.sh | awk 'NR==2{print $1}') != ""]; then
+if $(ps | grep restart.sh | awk 'NR==2{print $1}') != "" then
     echo "Killing restart.sh";
     kill -9 $(ps | grep restart.sh | awk 'NR==1{print $1}')
 fi
