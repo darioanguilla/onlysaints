d1be62dceb459deae98cfd573a417311aaa6f440
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 23:03:59 +0200

    modified if in restart and server

--- restart.sh
+++ restart.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-out="$(ps | grep nc)"
-if [[ -n $out]] then
     echo "Killing nc";
 	kill -9 $(ps | grep nc | awk '{print $1}')
-fi
-out="$(ps | grep server.sh)"
-if [[ -n $out]] then    
     echo "Killing server.sh";
     kill -9 $(ps | grep server.sh | awk '{print $1}')
-fi
-out="$(ps | grep start.sh)"
-if [[ -n $out]] then
     echo "Killing start.sh";
     kill -9 $(ps | grep start.sh | awk '{print $1}')
-fi
-out="$(ps | grep restart.sh)"
-if [[ -n $out]] then
     echo "Killing restart.sh";
     kill -9 $(ps | grep restart.sh | awk 'NR==1{print $1}')
-fi
-
 /onlysaints/start.sh
--- server.sh
+++ server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 while true; do
-    out="$(ps | grep nc)"
-    if [[-n $out]] then
-	    kill -9 $(ps -a | grep nc | awk '{print $1}')
-	fi
+	kill -9 $(ps -a | grep nc | awk '{print $1}')
 	printf "HTTP/1.0 200 OK\nContent-Length: `wc index.html | awk '{print $3-1}'`\n\n`cat index.html`" | nc -l 9191 > /dev/null
 done;
