7f7677fdb9dd3654065d86ed7d29c2aefd10fd9d
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 23:37:47 +0200

    modified restart

--- restart.sh
+++ restart.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-    echo "Killing nc"
-    kill -9 $(ps | grep nc | awk '{print $1}') && echo "nc killed"
+#    echo "Killing nc"
+#    kill -9 $(ps | grep nc | awk '{print $1}') && echo "nc killed"
     echo "Killing server.sh"
     kill -9 $(ps | grep server.sh | awk '{print $1}') && echo "server killed"
-	/onlysaints/update-motd.sh
-    /onlysaints/server.sh
-
+/onlysaints/start.sh
