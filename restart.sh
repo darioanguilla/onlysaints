f71920cfe1681701f306d136ce97ce73c022e211
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 23:18:15 +0200

    restart not kill start & restart

--- restart.sh
+++ restart.sh
@@ -3,8 +3,4 @@
     kill -9 $(ps | grep nc | awk '{print $1}') && echo "nc killed"
     echo "Killing server.sh"
     kill -9 $(ps | grep server.sh | awk '{print $1}') && echo "server killed"
-    echo "Killing start.sh"
-    kill -9 $(ps | grep start.sh | awk '{print $1}') && echo "start killed"
-    echo "Killing restart.sh"
-    kill -9 $(ps | grep restart.sh | awk 'NR==1{print $1}') && echo "restart killed"
 /onlysaints/start.sh
