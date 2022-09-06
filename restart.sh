4e2bd5c2e77ef6da0c3fa51f065a480d86c0cfea
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 11:22:48 +0200

    Fixed restart.sh & Dockerfile

--- restart.sh
+++ restart.sh
@@ -4,11 +4,15 @@ if ps -a | grep nc > /dev/null; then
 fi
 
 if ps -a | grep server.sh > /dev/null; then
-            kill -9 $(ps -a | grep nc | awk '{print $1}')
+            kill -9 $(ps -a | grep server.sh | awk '{print $1}')
 fi
 
 if ps -a | grep start.sh > /dev/null; then
-            kill -9 $(ps -a | grep nc | awk '{print $1}')
+            kill -9 $(ps -a | grep start.sh | awk '{print $1}')
+fi
+
+if ps -a | grep start.sh | awk 'NR==2{print $1}' > /dev/null; then
+            kill -9 $(ps -a | grep restart.sh | awk 'NR==1{print $1}')
 fi
 
 ./start.sh
