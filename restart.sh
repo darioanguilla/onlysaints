aa6db7cf844787e2ef9e924ecd2c2b53e361496f
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 19:45:48 +0200

    ps -au

--- restart.sh
+++ restart.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-if ps -a | grep nc > /dev/null; then
-	kill -9 $(ps -a | grep nc | awk '{print $1}')
+if ps -au | grep nc > /dev/null; then
+	kill -9 $(ps -au | grep nc | awk '{print $1}')
 fi
 
-if ps -a | grep server.sh > /dev/null; then
-            kill -9 $(ps -a | grep server.sh | awk '{print $1}')
+if ps -au | grep server.sh > /dev/null; then
+            kill -9 $(ps -au | grep server.sh | awk '{print $1}')
 fi
 
-if ps -a | grep start.sh > /dev/null; then
-            kill -9 $(ps -a | grep start.sh | awk '{print $1}')
+if ps -au | grep start.sh > /dev/null; then
+            kill -9 $(ps -au | grep start.sh | awk '{print $1}')
 fi
 
-if ps -a | grep restart.sh | awk 'NR==2{print $1}' > /dev/null; then
-            kill -9 $(ps -a | grep restart.sh | awk 'NR==1{print $1}')
+if ps -au | grep restart.sh | awk 'NR==2{print $1}' > /dev/null; then
+            kill -9 $(ps -au | grep restart.sh | awk 'NR==1{print $1}')
 fi
 
 ./start.sh
