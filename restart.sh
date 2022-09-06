5b19752befa2a4e84a5d8398c4855585d480261e
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-06 23:09:01 +0200

    modified if in restart and server

--- restart.sh
+++ restart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-    echo "Killing nc";
-	kill -9 $(ps | grep nc | awk '{print $1}')
-    echo "Killing server.sh";
-    kill -9 $(ps | grep server.sh | awk '{print $1}')
-    echo "Killing start.sh";
-    kill -9 $(ps | grep start.sh | awk '{print $1}')
-    echo "Killing restart.sh";
-    kill -9 $(ps | grep restart.sh | awk 'NR==1{print $1}')
+    echo "Killing nc"
+    kill -9 $(ps | grep nc | awk '{print $1}') && echo "nc killed"
+    echo "Killing server.sh"
+    kill -9 $(ps | grep server.sh | awk '{print $1}') && echo "server killed"
+    echo "Killing start.sh"
+    kill -9 $(ps | grep start.sh | awk '{print $1}') && echo "start killed"
+    echo "Killing restart.sh"
+    kill -9 $(ps | grep restart.sh | awk 'NR==1{print $1}') && echo "restart killed"
 /onlysaints/start.sh
