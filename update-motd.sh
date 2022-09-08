9d14e0a5a7ccc03930e2c0837b6d883e774cf10c
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-08 11:16:57 +0200

    update-mot in cron

--- update-motd.sh
+++ update-motd.sh
@@ -2,7 +2,7 @@
 export LC_TIME=it_IT.UTF-8
 COL=0
 /usr/bin/date +"%a, %d%b%Y" | /usr/bin/toilet -f big > /onlysaints/motd;
-COL=$(wc -L ./motd | cut -d " " -f 1);
+COL=$(wc -L /onlysaints/motd | awk '{print $1}');
 /usr/bin/echo "" >> /onlysaints/motd;
 /usr/bin/curl -k -s -H "Accept: application/json" https://www.vaticannews.va/it/santo-del-giorno/`/usr/bin/date +"%m/%d"`.saints.js | /usr/bin/jq -r '.saints[] | "\"\(.name)\"\n\n\(.summary)\n"'| konwert UTF8-ascii/it | fold -w$COL | awk '{$1=$1};1' >> /onlysaints/motd;
 /usr/bin/cat /onlysaints/motd | /usr/bin/boxes -d parchment > /onlysaints/sotd;
