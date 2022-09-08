fab97ea4c351f1394e09e6f4cbf5d6a7ed5732f1
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-08 11:51:44 +0200

    beutified log in start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-printf "Killing nc"
-kill -9 $(ps | grep nc | awk '{print $1}') && printf "nc killed"
-printf "Killing server.sh"
-kill -9 $(ps | grep server.sh | awk '{print $1}') && printf "server killed"
+printf "\nKilling nc\n"
+kill -9 $(ps | grep nc | awk '{print $1}') && printf "\nnc killed\n"
+printf "\nKilling server.sh\n"
+kill -9 $(ps | grep server.sh | awk '{print $1}') && printf "\nserver.sh killed\n"
 /onlysaints/update-motd.sh
 /onlysaints/server.sh
