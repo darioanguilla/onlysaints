cee3ce16f73326bb16c6142e346a572bf7062bd7
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-08 16:33:21 +0200

    curl verify ssl

--- update-motd.sh
+++ update-motd.sh
@@ -4,6 +4,6 @@ COL=0
 /usr/bin/date +"%a, %d%b%Y" | /usr/bin/toilet -f big > /onlysaints/motd;
 COL=$(wc -L /onlysaints/motd | awk '{print $1}');
 /usr/bin/echo "" >> /onlysaints/motd;
-/usr/bin/curl -k -s -H "Accept: application/json" https://www.vaticannews.va/it/santo-del-giorno/`/usr/bin/date +"%m/%d"`.saints.js | /usr/bin/jq -r '.saints[] | "\"\(.name)\"\n\n\(.summary)\n"'| konwert UTF8-ascii/it | fold -w$COL | awk '{$1=$1};1' >> /onlysaints/motd;
+/usr/bin/curl -s -H "Accept: application/json" https://www.vaticannews.va/it/santo-del-giorno/`/usr/bin/date +"%m/%d"`.saints.js | /usr/bin/jq -r '.saints[] | "\"\(.name)\"\n\n\(.summary)\n"'| konwert UTF8-ascii/it | fold -w$COL | awk '{$1=$1};1' >> /onlysaints/motd;
 /usr/bin/cat /onlysaints/motd | /usr/bin/boxes -d parchment > /onlysaints/sotd;
 /onlysaints/update-saints.sh
