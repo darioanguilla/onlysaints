6ed9e5540a439b91230930c60885820095d6f655
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-10 18:58:09 +0200

    complete index w all html5 tags

--- update-saints.sh
+++ update-saints.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/echo "<pre>" > /onlysaints/index.html
+/usr/bin/cat /onlysaints/index.ante > /onlysaints/index.html
 /usr/bin/cat /onlysaints/sotd | sed 's/</\&lt;/g' | sed 's/>/\&gt;/g' >> /onlysaints/index.html
-/usr/bin/echo "</pre>" >> /onlysaints/index.html
+/usr/bin/cat /onlysaints/index.post >> /onlysaints/index.html
 
