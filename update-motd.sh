4276b8941060d767c054bb3a50bb865eb5efc030
Dario Anguilla <dario.anguilla@gmail.com>
2022-09-12 00:34:33 +0200

    utf8

--- update-motd.sh
+++ update-motd.sh
@@ -5,5 +5,5 @@ COL=0
 COL=$(wc -L /onlysaints/motd | awk '{print $1}');
 /usr/bin/echo "" >> /onlysaints/motd;
 /usr/bin/curl -s -H "Accept: application/json" https://www.vaticannews.va/it/santo-del-giorno/`/usr/bin/date +"%m/%d"`.saints.js | /usr/bin/jq -r '.saints[] | "\"\(.name)\"\n\n\(.summary)\n"'| fold -w$COL | awk '{$1=$1};1' >> /onlysaints/motd;
-/usr/bin/cat /onlysaints/motd | /usr/bin/boxes -d parchment > /onlysaints/sotd;
+/usr/bin/cat /onlysaints/motd | /usr/bin/boxes -n UTF-8 -d parchment > /onlysaints/sotd;
 /onlysaints/update-saints.sh
